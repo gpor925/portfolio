76dac218006a1b4fc3a224a96f7dc58d3d1f8081
gpor925 <gpor925@g.educaand.es>
2024-12-10 02:58:21 +0100

    Foto perfil y demoreel

--- source.js
+++ source.js
@@ -15,7 +15,7 @@ let contactoText = document.getElementById("contactoText")
 let traducido = false
 
 function traduccion() {
-    if (traducido == true) {
+    if (traducido == false) {
         portfolioNav.innerHTML = "Portfolio"
         contactoNav.innerHTML = "Contact"
         bioText.innerHTML = "I'm George, I'm 23 years old and I'm a 2D and 3D Animation student in IES Ángel de Saavedra (Córdoba). I have a passion for animation, and I'd like to specialise in both 2D and 3D animation. No matter what I'm working on, I work hard and I'll give it my all. I'm not afraid to suggest new ideas or to learn from my mistakes. All in all, I can work on anything, be it modeling, texturing, rigging or animation."
@@ -29,13 +29,13 @@ function traduccion() {
         modelajeText.innerHTML = "Modeling"
         arteText.innerHTML = "Art"
         contactoText.innerHTML = "Contact"
+        traducido = true
 
-        traducido = false
-    } else if (traducido == false){
+    } else if (traducido == true){
         portfolioNav.innerHTML = "Portfolio"
         contactoNav.innerHTML = "Contacto"
-        bioText.innerHTML = "Soy George, tengo 23 años y soy estudiante de Animaciones 2D y3D, juegos y entornos interactivos en el IES Ángel de Saavedra (Córdoba). Mi pasión es la animación, y busco especializarme en el apartado de animación tanto 2D como 3D. En cualquier proyecto soy una persona trabajadora y dedicada, y no tengo miedo a aportar ideas y de aprender de mis errores. No obstante, soy capaz de trabajar en cualquier ámbito, ya sea modelaje, texturizado, rigging, o animación."
-        sobreMi.innerHTML = "Sobre Mí"
+        bioText.innerHTML = "Soy George, tengo 23 años y soy estudiante de Animaciones 2D y 3D, juegos y entornos interactivos en el IES Ángel de Saavedra (Córdoba). Mi pasión es la animación, y busco especializarme en el apartado de animación tanto 2D como 3D. En cualquier proyecto soy una persona trabajadora y dedicada, y no tengo miedo a aportar ideas y de aprender de mis errores. No obstante, soy capaz de trabajar en cualquier ámbito, ya sea modelaje, texturizado, rigging, o animación."
+        sobreMi.innerHTML = "Sobre mí"
         fotoDemoreel.src = "img/demoreel-thumb.png"
         fotoAnimacion.src = "img/animacion-thumb.png"
         fotoModelaje.src = "img/modelaje-thumb.png"
@@ -45,8 +45,8 @@ function traduccion() {
         modelajeText.innerHTML = "Modelaje"
         arteText.innerHTML = "Arte"
         contactoText.innerHTML = "Contacto"
+        traducido = false
 
-        traducido = true
     }
 }
 
